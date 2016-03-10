cfcbf8fe8cd7370c88175d19b92a81876b926876
justindomingue <justin.domingue@hotmail.com>
2016-03-10 18:41:28 -0500

    implemented Kolmogorov-Smirnov test

--- data_analyzer.py
+++ data_analyzer.py
@@ -1,4 +1,25 @@
+from scipy import stats
+import pandas as pd
+
 class DataAnalyzer:
 
-    def likelihood_ratio_test(self, df1, df2):
-        return True
+   def kstest(self, df1, df2):
+      '''Performs a Kolmogorov-Smirnov test on originator and claimant
+      :param df1 [DataFrame] Observations
+      '''
+
+      ks = 0
+      pval = 0
+      print('--')
+      for i in df1:
+         if i in df1.columns.values and i in df2.columns.values:
+            ksstat = stats.ks_2samp(df1[i],df2[i])
+            print(i, ksstat)
+            # ks+=ksstat[0]
+            # pval += ksstat[1]
+      # print('ks:{0}, p-value:{1}'.format(ks,pval))
+
+      # print(df1[0])
+      # stats.ks_2samp(df1, df2)
+
+
--- data_processor.py
+++ data_processor.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import codecs
 import pandas as pd
 import json
 
@@ -6,8 +7,8 @@ from data_analyzer import DataAnalyzer
 
 
 MIN_OBS      = 5
-MIN_DATA_LEN = 100
-MIN_DELTA    = 500
+MIN_DATA_LEN = 200
+MIN_DELTA    = 5000
 NGRAMS_SIZE  = 2
 
 class DataProcessor:
@@ -29,7 +30,7 @@ class DataProcessor:
         #   data = [ [key, DOWN/UP, timestamp], ... ]
 
         if len(data) < MIN_DATA_LEN:
-            print("Data set contains less than 200 points.")
+            print("Data set contains less than {0} points.".format(MIN_DATA_LEN))
             print("Continuing nonetheless.")
 
         # Get ngrams
@@ -47,8 +48,13 @@ class DataProcessor:
         self.digraphs = digraphs
 
         # Create panda frame
-        df = pd.DataFrame({k: pd.Series(v) for k,v in digraphs.items()})
-        self.preprocessed = df.sort_index()
+        #
+        # idx   digraph1    digraph2    ...     digraphN
+        # 0
+        #
+        df = pd.DataFrame({k: pd.Series(v) for k,v in digraphs.items() if len(v) > MIN_OBS })
+
+        self.preprocessed = df
 
     def process(self):
         ''' Processes the data by extracting statistical functions into self.processed (dataframe)
@@ -66,10 +72,31 @@ class DataProcessor:
         # Filter
         df = df[df['count'] >= MIN_OBS]
 
+        # At this point, df looks like this:
+        #
+        # digraph   count   mean    variance
+        # ----------------------------------
+        # ar        5       95.4    585.8       <- assumed to be normal
+        # di        8       74.5    291.0
+        # ...
+        #
+
         # Normalize data
         # TODO
 
         self.processed = df
+
+    def normalize(self, columns_names):
+        '''Filters columns not in `column_names`'''
+        return self.preprocessed[columns_names]
+
+    @property
+    def observations(self):
+        return self.preprocessed
+
+    @property
+    def columns(self):
+        return list(self.preprocessed.columns.values)
 
     def filter(self, data, action):
         return [x for x in data if x[1] == action]
@@ -77,48 +104,121 @@ class DataProcessor:
     def ngrams(self, lst, n=2):
         return zip(*[lst[i:] for i in range(n)])
 
+    @staticmethod
+    def from_file(filename, user=None):
+        ret = []
+
+        with codecs.open(filename,'r', encoding='utf-8') as f:
+            data = json.load(f)
+
+            samples = data['data']
+
+            for sample in samples:
+                if user == None or sample['user'] == user:
+                    dp = DataProcessor(sample)
+                    dp.preprocess()
+                    dp.process()
+
+                    ret.append(dp.observations)
+
+        return ret
+
+
+
+import numpy as np
+from scipy import stats
 
 if __name__ == "__main__":
-    with open('data/catherine','r') as f:
-        data = json.load(f)
+    dp_domingue = DataProcessor.from_file('data/domingue.json')
+    dp_dan = DataProcessor.from_file('data/dan.json')
 
-        # samples is a list of dictionaries {'user':<user>, 'data':[ [key,up/down,time] ]}
-        samples = data['data']
+    analyzer = DataAnalyzer()
 
-        dp1 = DataProcessor(samples[0])
-        dp1.preprocess()
-        dp1.process()
+    print("same")
+    analyzer.kstest(dp_domingue[0], dp_domingue[1])
+    analyzer.kstest(dp_dan[0], dp_dan[1])
+    analyzer.kstest(dp_dan[0], dp_dan[2])
+    analyzer.kstest(dp_dan[0], dp_dan[3])
+    analyzer.kstest(dp_dan[1], dp_dan[3])
+    analyzer.kstest(dp_dan[2], dp_dan[3])
 
-        dp2 = DataProcessor(samples[1])
-        dp2.preprocess()
-        dp2.process()
+    print("\ndifferent")
+    analyzer.kstest(dp_domingue[0], dp_dan[0])
+    analyzer.kstest(dp_domingue[1], dp_dan[1])
+    analyzer.kstest(dp_domingue[1], dp_dan[2])
+    analyzer.kstest(dp_domingue[0], dp_dan[3])
 
-        analyzer = DataAnalyzer()
+    #
+    # with codecs.open('data/catherine','r', encoding='utf-8') as f:
+    #     data = json.load(f)
+    #
+    #     # samples is a list of dictionaries {'user':<user>, 'data':[ [key,up/down,time] ]}
+    #     samples = data['data']
+    #
+    #     dp1 = DataProcessor(samples[0])
+    #     dp1.preprocess()
+    #     dp1.process()
+    #
+    #     dp2 = DataProcessor(samples[1])
+    #     dp2.preprocess()
+    #     dp2.process()
+    #
+    #     dp4 = DataProcessor(samples[2])
+    #     dp4.preprocess()
+    #     dp4.process()
+    #
+    # with codecs.open('data/justin','r',encoding='utf-8') as f:
+    #     data = json.load(f)
+    #
+    #     # samples is a list of dictionaries {'user':<user>, 'data':[ [key,up/down,time] ]}
+    #     samples = data['data']
+    #
+    #     dp3 = DataProcessor(samples[0])
+    #     dp3.preprocess()
+    #     dp3.process()
+    #
+    # print(dp1.observations)
+    # print(dp2.observations)
+    # print(dp3.observations)
+    #
+    # analyzer = DataAnalyzer()
+    # print('1-1')
+    # analyzer.kstest(dp1.observations, dp1.observations)
+    # print('1-2')
+    # analyzer.kstest(dp1.observations, dp2.observations)
+    # print('2-3')
+    # analyzer.kstest(dp2.observations, dp3.observations)
+    # print('1-3')
+    # analyzer.kstest(dp1.observations, dp3.observations)
+    # print('1-4')
+    # analyzer.kstest(dp1.observations, dp4.observations)
+    # print('3-4')
+    # analyzer.kstest(dp3.observations, dp4.observations)
 
-        # all_means = pd.DataFrame()
-        # for k,v in data.items():
-        #     print(k)
-        #
-        #     dp = DataProcessor(v)
-        #     dp.preprocess()
-        #     dp.process()
-        #
-        #     # print(dp.df)
-        #     all_means = pd.concat([all_means, dp.df.loc[:,1:2]], axis=1, join='inner')
-        #
-        # print(all_means)
-        # # all_means_wo_na = all_means.dropna()
+    # all_means = pd.DataFrame()
+    # for k,v in data.items():
+    #     print(k)
+    #
+    #     dp = DataProcessor(v)
+    #     dp.preprocess()
+    #     dp.process()
+    #
+    #     # print(dp.df)
+    #     all_means = pd.concat([all_means, dp.df.loc[:,1:2]], axis=1, join='inner')
+    #
+    # print(all_means)
+    # # all_means_wo_na = all_means.dropna()
 
-        # print(all_means_wo_na)
-        #
-        # for i in range(0,all_means_wo_na.shape[1],2):
-        #     for j in range(0,all_means_wo_na.shape[1],2):
-        #         print("{0} {1}".format(all_means_wo_na.loc[i], all_means_wo_na.loc[j]))
-                # dist = seuclidean(all_means_wo_na.values[i], all_means_wo_na.values[j], all_means_wo_na.values[i+1])
-                # print("{0},{1} {2}".format(i,j,dist))
+    # print(all_means_wo_na)
+    #
+    # for i in range(0,all_means_wo_na.shape[1],2):
+    #     for j in range(0,all_means_wo_na.shape[1],2):
+    #         print("{0} {1}".format(all_means_wo_na.loc[i], all_means_wo_na.loc[j]))
+            # dist = seuclidean(all_means_wo_na.values[i], all_means_wo_na.values[j], all_means_wo_na.values[i+1])
+            # print("{0},{1} {2}".format(i,j,dist))
 
-        # print(all_means.dropna())
-        # print(all_means)
+    # print(all_means.dropna())
+    # print(all_means)
 
-    # print(euclidean(frames[0][1][0:200], frames[1][1][0:200]))
+# print(euclidean(frames[0][1][0:200], frames[1][1][0:200]))
 
