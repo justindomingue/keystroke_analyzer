92496b884e1322df878748c4e5a3acd74c2baebb
justindomingue <justin.domingue@hotmail.com>
2016-03-11 23:37:33 -0500

    1.0.3

--- analyzer/data_analyzer.py
+++ analyzer/data_analyzer.py
@@ -6,30 +6,21 @@ class DataAnalyzer:
    def compare(self, sample1, sample2, test):
       '''Compares the underlying distribution of two samples
 
+       :param test One of 'k-s' or //TODO
        :returns True is `sample1` is likely to be from the same distribution as `sample2`
        '''
-      if (test.starts_with('ks')):
+      if (test.startswith('k-s')):
          return self.kstest(sample1, sample2)
-
-
 
    def kstest(self, sample1, sample2):
       '''Performs a Kolmogorov-Smirnov test on originator and claimant
       :param sample1 [DataFrame] Observations
       '''
 
-      ks = 0
-      pval = 0
-      print('--')
+      results = []
       for i in sample1:
          if i in sample1.columns.values and i in sample2.columns.values:
             ksstat = stats.ks_2samp(sample1[i],sample2[i])
-            print(i, ksstat)
-            # ks+=ksstat[0]
-            # pval += ksstat[1]
-      # print('ks:{0}, p-value:{1}'.format(ks,pval))
-
-      # print(sample1[0])
-      # stats.ks_2samp(sample1, sample2)
-
+            results.append(ksstat)
+      return results
 
